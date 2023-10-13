98e774dda02ffaae2cdf3bd32aa1c437a00ef6e5
ahdianmirza <ahdianmirza@gmail.com>
2023-10-13 15:12:52 +0700

    Update Todos

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -16,6 +16,11 @@ class TodoList extends Component
 
     public $search;
 
+    public $editingTodoID;
+
+    #[Rule('required|min:3|max:50')]
+    public $editingTodoName;
+
     public function create() {
         // validate
         $validated = $this->validateOnly('name');
@@ -38,6 +43,25 @@ class TodoList extends Component
         $todo = Todo::find($todoID);
         $todo->completed = !$todo->completed;
         $todo->save();
+    }
+
+    public function edit($todoID) {
+        $this->editingTodoID = $todoID;
+        $this->editingTodoName = Todo::find($todoID)->name;
+    }
+
+    public function cancelEdit() {
+        $this->reset('editingTodoID', 'editingTodoName');
+    }
+
+    public function update() {
+        $this->validateOnly('editingTodoName');
+
+        Todo::find($this->editingTodoID)->update([
+            'name' => $this->editingTodoName
+        ]);
+
+        $this->cancelEdit();
     }
 
     public function render()
